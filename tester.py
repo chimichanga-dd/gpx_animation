487d673dff6cc4e5a0fde01329a225aa425b396a
David Dixon <chimichanga@g.ucla.edu>
2023-06-07 08:47:12 -0700

    file cleanup

--- tester.py
+++ tester.py
@@ -4,39 +4,61 @@ import matplotlib.pyplot as plt
 from geodatasets import get_path
 import contextily as cx
 import gpx_parser
-
-
-# a = [1,2,3,4]
-# step = 2
-# for idx in range(0,len(a),step):
-#     print(a[idx: idx+step])
+import os
+import imageio
 
 
 activity_file_path = "/Users/daviddixon/Documents/Code/plotter/6189805369.gpx"
 activity_file_path = "/Users/daviddixon/Documents/Code/plotter/8754420234.gpx"
 gpx_data = gpx_parser.parse(activity_file_path)
-# print(gpx_data)
+step = 20
+file_names = []
+path = "./images"
 
-# for idx in range(len(gpx_data["points"])):
-df = pd.DataFrame(
-    {
-        # "City": ["Buenos Aires", "Brasilia", "Santiago", "Bogota", "Caracas"],
-        # "Country": ["Argentina", "Brazil", "Chile", "Colombia", "Venezuela"],
-        "Latitude": gpx_data["lats"],
-        "Longitude": gpx_data["lons"],
-    }
-)
+#create image directory
+isExist = os.path.exists(path)
+if not isExist:
+   os.makedirs(path)
 
-gdf = geopandas.GeoDataFrame(
-    df, geometry=geopandas.points_from_xy(df.Longitude, df.Latitude), crs="EPSG:4326"
-)
+#create images
+for idx in range(0, len(gpx_data["points"]), step):
+    prev_df = pd.DataFrame(
+        {
+            "Latitude": gpx_data["lats"][0: idx] if idx > step else [],
+            "Longitude": gpx_data["lons"][0: idx] if idx > step else [],
+        }
+    )
+    old_gdf = geopandas.GeoDataFrame(
+        prev_df, geometry=geopandas.points_from_xy(prev_df.Longitude, prev_df.Latitude), crs="EPSG:4326",
+        
+    )
+    old_ax = old_gdf.plot(markersize=2, color="red", aspect=1)
 
-# print(gdf.head())
+    new_df = pd.DataFrame(
+        {
+            "Latitude": gpx_data["lats"][idx: idx + step],
+            "Longitude": gpx_data["lons"][idx: idx + step],
+        }
+    )
+    gdf = geopandas.GeoDataFrame(
+        new_df, geometry=geopandas.points_from_xy(new_df.Longitude, new_df.Latitude), crs="EPSG:4326"
+    )
+    ax = gdf.plot(ax=old_ax,markersize=2, color="blue")
 
-# # We can now plot our ``GeoDataFrame``.
-# # gdf.plot(ax=ax, color="red")
-ax = gdf.plot(color="red")
-cx.add_basemap(ax, crs=gdf.crs.to_string())
-# plt.text(lon, lat ,'MY SUPER TITLE')
-# plt.show()
-plt.savefig('foo.png')
+    ax.set_xlim(gpx_data["min_lon"], gpx_data["max_lon"])
+    ax.set_ylim(gpx_data["min_lat"], gpx_data["max_lat"])
+    ax.axis('off')
+    ax.tick_params(left=False, labelleft=False, bottom=False, labelbottom=False)
+
+    cx.add_basemap(ax, crs=gdf.crs.to_string(), attribution="")
+    print(f"saving image {idx / 20} of {len(gpx_data['points']) / 20}")
+    image_file_path = f'{path}/foo{idx}.png'
+    plt.savefig(image_file_path, bbox_inches='tight', pad_inches=0)
+    file_names.append(image_file_path)
+    plt.close()
+
+#create gif using images
+with imageio.get_writer('./gpx_combined.gif', mode='I') as writer:
+    for filename in file_names:
+        image = imageio.imread(filename)
+        writer.append_data(image)
