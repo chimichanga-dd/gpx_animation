a5a0c51f04b039a869622ab138f20771890221ca
David Dixon <chimichanga@g.ucla.edu>
2023-06-07 15:49:16 -0700

    update for multiple gpx files

--- gpx_parser.py
+++ gpx_parser.py
@@ -2,68 +2,94 @@ import re
 import argparse
 import xml.etree.ElementTree as et
 import math
+from datetime import datetime, timedelta
 
-# takes in a GPX file and outputs a CSV file
+
+def time_in_seconds(date):
+    for fmt in ("%Y-%m-%dT%H:%M:%S.%fZ", "%Y-%m-%dT%H:%M:%SZ"):
+        try:
+            datetime.strptime(date, fmt)
+            time_object = datetime.strptime(date, fmt)
+            return timedelta(
+                hours=time_object.hour,
+                minutes=time_object.minute,
+                seconds=time_object.second,
+            ).total_seconds()
+        except ValueError:
+            pass
+    raise ValueError("no valid date format found")
 
 
-def parse(input):
+def step_floor(x, base=20):
+    return base * math.floor(x / base)
+
+
+def parse(input, combined_gpx_attributes):
     tree = et.parse(input)
     root = tree.getroot()
-    m = re.match(r'^({.*})', root.tag)
+    m = re.match(r"^({.*})", root.tag)
     if m:
         ns = m.group(1)
     else:
-        ns = ''
-    if root.tag != ns+'gpx':
-        print('Unknown root found: '+root.tag)
+        ns = ""
+    if root.tag != ns + "gpx":
+        print("Unknown root found: " + root.tag)
         return
-    trk = root.find(ns+'trk')
+    trk = root.find(ns + "trk")
     if not trk:
-        print('Unable to find trk under root')
+        print("Unable to find trk under root")
         return
     trksegs = trk.findall(ns + "trkseg")
     if not trksegs:
         print("Unable to find trksegs under trk")
         return
-    #loop over each trksegment
-
-    gpx_attributes = {
-        "min_lat": math.inf,
-        "max_lat": -math.inf,
-        "min_lon": math.inf,
-        "max_lon": -math.inf,
-        "points": [],
-        "lats": [],
-        "lons": []
-    }
 
     earliest_point = None
 
     for trkseg in trksegs:
-        trkpts = trkseg.findall(ns+'trkpt')
+        trkpts = trkseg.findall(ns + "trkpt")
         if not trkpts:
-            print('Unable to find trkpts under trkseg')
+            print("Unable to find trkpts under trkseg")
             return
         for trkpt in trkpts:
             if not trkpt:
                 print("missing trkpt")
-            # print(trkpt)
-            track_point_time_element = trkpt.find(ns+'time')
-            # print(track_point_time_element, track_point_time_element.text)
+            track_point_time_element = trkpt.find(ns + "time")
             track_point = {
-                "lat": float(trkpt.attrib['lat']),
-                "lon": float(trkpt.attrib['lon']),
+                "lat": float(trkpt.attrib["lat"]),
+                "lon": float(trkpt.attrib["lon"]),
                 "time": track_point_time_element.text,
             }
 
-            gpx_attributes["lats"].append(track_point["lat"])
-            gpx_attributes["lons"].append(track_point["lon"])
-            gpx_attributes["min_lat"] = min(gpx_attributes["min_lat"], track_point["lat"])
-            gpx_attributes["max_lat"] = max(gpx_attributes["max_lat"], track_point["lat"])
-            gpx_attributes["min_lon"] = min(gpx_attributes["min_lon"], track_point["lon"])
-            gpx_attributes["max_lon"] = max(gpx_attributes["max_lon"], track_point["lon"])
+            if earliest_point is None:
+                earliest_point = time_in_seconds(track_point["time"])
 
-            gpx_attributes["points"].append(track_point)
-    return gpx_attributes
+            elapsed_time = abs(earliest_point - time_in_seconds(track_point["time"]))
+            time_bucket = step_floor(elapsed_time)
 
+            if time_bucket not in combined_gpx_attributes["lats"]:
+                combined_gpx_attributes["lats"][time_bucket] = []
+            if time_bucket not in combined_gpx_attributes["lons"]:
+                combined_gpx_attributes["lons"][time_bucket] = []
 
+            combined_gpx_attributes["lats"][time_bucket].append(track_point["lat"])
+            combined_gpx_attributes["lons"][time_bucket].append(track_point["lon"])
+            combined_gpx_attributes["min_lat"] = min(
+                combined_gpx_attributes["min_lat"], track_point["lat"]
+            )
+            combined_gpx_attributes["max_lat"] = max(
+                combined_gpx_attributes["max_lat"], track_point["lat"]
+            )
+            combined_gpx_attributes["min_lon"] = min(
+                combined_gpx_attributes["min_lon"], track_point["lon"]
+            )
+            combined_gpx_attributes["max_lon"] = max(
+                combined_gpx_attributes["max_lon"], track_point["lon"]
+            )
+            combined_gpx_attributes["last_step"] = max(
+                combined_gpx_attributes["last_step"], time_bucket
+            )
+
+            combined_gpx_attributes["points"].append(track_point)
+
+    return combined_gpx_attributes
--- tester.py
+++ tester.py
@@ -8,26 +8,52 @@ import imageio.v3 as iio
 from pygifsicle import optimize
 import numpy as np
 import imageio.v3 as iio
+import math
 
 
-activity_file_path = "/Users/daviddixon/Documents/Code/plotter/6189805369.gpx"
-activity_file_path = "/Users/daviddixon/Documents/Code/plotter/8754420234.gpx"
-gpx_data = gpx_parser.parse(activity_file_path)
 step = 20
 file_names = []
-path = "./images"
+images_folder = "./images"
+combined_gpx_attributes = {
+    "min_lat": math.inf,
+    "max_lat": -math.inf,
+    "min_lon": math.inf,
+    "max_lon": -math.inf,
+    "last_step": 0,
+    "points": [],
+    "lats": {},
+    "lons": {},
+}
+
+activities = [
+    "/Users/daviddixon/Documents/Code/plotter/6189805369.gpx",
+    "/Users/daviddixon/Documents/Code/plotter/8754420234.gpx",
+]
+
+for activity in activities:
+    gpx_parser.parse(activity, combined_gpx_attributes)
 
 # create image directory
-isExist = os.path.exists(path)
+isExist = os.path.exists(images_folder)
 if not isExist:
-    os.makedirs(path)
+    os.makedirs(images_folder)
+
+
+previous_tracks = {"lats": [], "lons": []}
 
 # create images
-for idx in range(0, len(gpx_data["points"]), step):
+for idx in range(0, combined_gpx_attributes["last_step"], step):
+    if (
+        idx not in combined_gpx_attributes["lats"]
+        or idx not in combined_gpx_attributes["lons"]
+    ):
+        continue
+
+    # plot previous tracks
     prev_df = pd.DataFrame(
         {
-            "Latitude": gpx_data["lats"][0:idx] if idx > step else [],
-            "Longitude": gpx_data["lons"][0:idx] if idx > step else [],
+            "Latitude": previous_tracks["lats"],
+            "Longitude": previous_tracks["lons"],
         }
     )
     old_gdf = geopandas.GeoDataFrame(
@@ -37,10 +63,14 @@ for idx in range(0, len(gpx_data["points"]), step):
     )
     old_ax = old_gdf.plot(markersize=2, color="red", aspect=1)
 
+    # plot current tracks
+    current_lats = combined_gpx_attributes["lats"][idx]
+    current_lons = combined_gpx_attributes["lons"][idx]
+
     new_df = pd.DataFrame(
         {
-            "Latitude": gpx_data["lats"][idx : idx + step],
-            "Longitude": gpx_data["lons"][idx : idx + step],
+            "Latitude": current_lats,
+            "Longitude": current_lons,
         }
     )
     gdf = geopandas.GeoDataFrame(
@@ -50,23 +80,55 @@ for idx in range(0, len(gpx_data["points"]), step):
     )
     ax = gdf.plot(ax=old_ax, markersize=2, color="blue")
 
-    ax.set_xlim(gpx_data["min_lon"], gpx_data["max_lon"])
-    ax.set_ylim(gpx_data["min_lat"], gpx_data["max_lat"])
+    # handle image layout
+    margin_percent = 0.05
+    plot_border_x_min = combined_gpx_attributes["min_lon"] + (
+        (combined_gpx_attributes["max_lon"] - combined_gpx_attributes["min_lon"])
+        * margin_percent
+        * (-1 if combined_gpx_attributes["min_lon"] < 0 else 1)
+    )
+    plot_border_x_max = combined_gpx_attributes["max_lon"] - (
+        (combined_gpx_attributes["max_lon"] - combined_gpx_attributes["min_lon"])
+        * margin_percent
+        * (-1 if combined_gpx_attributes["max_lon"] < 0 else 1)
+    )
+
+    plot_border_y_min = combined_gpx_attributes["min_lat"] - (
+        (combined_gpx_attributes["max_lat"] - combined_gpx_attributes["min_lat"])
+        * margin_percent
+        * (-1 if combined_gpx_attributes["min_lat"] < 0 else 1)
+    )
+    plot_border_y_max = combined_gpx_attributes["max_lat"] + (
+        (combined_gpx_attributes["max_lat"] - combined_gpx_attributes["min_lat"])
+        * margin_percent
+        * (-1 if combined_gpx_attributes["max_lat"] < 0 else 1)
+    )
+
+    ax.set_xlim(plot_border_x_min, plot_border_x_max)
+    ax.set_ylim(plot_border_y_min, plot_border_y_max)
     ax.axis("off")
     ax.tick_params(left=False, labelleft=False, bottom=False, labelbottom=False)
 
-    cx.add_basemap(ax, crs=gdf.crs.to_string(), attribution="")
-    print(f"saving image {idx / 20} of {len(gpx_data['points']) / 20}")
-    image_file_path = f"{path}/foo{idx}.png"
+    # print(retval)
+    cx.add_basemap(
+        ax,
+        crs=gdf.crs.to_string(),
+        attribution="",
+    )
+
+    # save image
+    print(
+        f"saving plot for step {int(idx / step + 1)} of {int(combined_gpx_attributes['last_step'] / step)}"
+    )
+    image_file_path = f"{images_folder}/foo{idx}.png"
     plt.savefig(image_file_path, bbox_inches="tight", pad_inches=0)
-    file_names.append(image_file_path)
     plt.close()
 
-# create gif using images
-# with imageio.get_writer('./gpx_combined.gif', mode='I') as writer:
-#     for filename in file_names:
-#         image = imageio.imread(filename)
-#         writer.append_data(image)
+    file_names.append(image_file_path)
+
+    # move current tracks into previous tracks
+    previous_tracks["lats"].extend(current_lats)
+    previous_tracks["lons"].extend(current_lons)
 
 
 gif_path = "./gpx_combined.gif"
