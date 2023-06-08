a15b5b50527c24f40dc951eed1c0a3fdba974756
David Dixon <chimichanga@g.ucla.edu>
2023-06-08 16:13:17 -0700

    re-write using scatter

--- tester.py
+++ tester.py
@@ -1,5 +1,4 @@
-import pandas as pd
-import geopandas
+import geopandas as gpd
 import matplotlib.pyplot as plt
 import contextily as cx
 import gpx_parser
@@ -9,6 +8,10 @@ from pygifsicle import optimize
 import numpy as np
 import imageio.v3 as iio
 import math
+import time
+
+
+start_time = time.time()
 
 
 step = 20
@@ -38,8 +41,53 @@ isExist = os.path.exists(images_folder)
 if not isExist:
     os.makedirs(images_folder)
 
+# handle image layout
+margin_percent = 0.05
+plot_border_x_min = combined_gpx_attributes["min_lon"] + (
+    (combined_gpx_attributes["max_lon"] - combined_gpx_attributes["min_lon"])
+    * margin_percent
+    * (-1 if combined_gpx_attributes["min_lon"] < 0 else 1)
+)
+plot_border_x_max = combined_gpx_attributes["max_lon"] - (
+    (combined_gpx_attributes["max_lon"] - combined_gpx_attributes["min_lon"])
+    * margin_percent
+    * (-1 if combined_gpx_attributes["max_lon"] < 0 else 1)
+)
 
-previous_tracks = {"lats": [], "lons": []}
+plot_border_y_min = combined_gpx_attributes["min_lat"] - (
+    (combined_gpx_attributes["max_lat"] - combined_gpx_attributes["min_lat"])
+    * margin_percent
+    * (-1 if combined_gpx_attributes["min_lat"] < 0 else 1)
+)
+plot_border_y_max = combined_gpx_attributes["max_lat"] + (
+    (combined_gpx_attributes["max_lat"] - combined_gpx_attributes["min_lat"])
+    * margin_percent
+    * (-1 if combined_gpx_attributes["max_lat"] < 0 else 1)
+)
+
+fig, ax = plt.subplots()
+crs_projection = "EPSG:4326"
+within_1_previous_tracks = {"lats": [], "lons": []}
+after_1_previous_tracks = {"lats": [], "lons": []}
+
+
+
+def plot_points(axis, lats, lons, color="red"):
+    if lats and lons:
+        gdf = gpd.GeoDataFrame(
+            geometry=gpd.points_from_xy(
+                lons,
+                lats,
+            ),
+            crs=crs_projection,
+        )
+
+        xy = gdf["geometry"].map(lambda point: point.xy)
+        x, y = zip(*xy)
+
+        axis.scatter(x=x, y=y, color=color, s=4)
+    return axis
+
 
 # create images
 for idx in range(0, combined_gpx_attributes["last_step"], step):
@@ -49,72 +97,41 @@ for idx in range(0, combined_gpx_attributes["last_step"], step):
     ):
         continue
 
-    # plot previous tracks
-    prev_df = pd.DataFrame(
-        {
-            "Latitude": previous_tracks["lats"],
-            "Longitude": previous_tracks["lons"],
-        }
-    )
-    old_gdf = geopandas.GeoDataFrame(
-        prev_df,
-        geometry=geopandas.points_from_xy(prev_df.Longitude, prev_df.Latitude),
-        crs="EPSG:4326",
-    )
-    old_ax = old_gdf.plot(markersize=2, color="red", aspect=1)
-
-    # plot current tracks
-    current_lats = combined_gpx_attributes["lats"][idx]
-    current_lons = combined_gpx_attributes["lons"][idx]
-
-    new_df = pd.DataFrame(
-        {
-            "Latitude": current_lats,
-            "Longitude": current_lons,
-        }
-    )
-    gdf = geopandas.GeoDataFrame(
-        new_df,
-        geometry=geopandas.points_from_xy(new_df.Longitude, new_df.Latitude),
-        crs="EPSG:4326",
-    )
-    ax = gdf.plot(ax=old_ax, markersize=2, color="blue")
-
-    # handle image layout
-    margin_percent = 0.05
-    plot_border_x_min = combined_gpx_attributes["min_lon"] + (
-        (combined_gpx_attributes["max_lon"] - combined_gpx_attributes["min_lon"])
-        * margin_percent
-        * (-1 if combined_gpx_attributes["min_lon"] < 0 else 1)
-    )
-    plot_border_x_max = combined_gpx_attributes["max_lon"] - (
-        (combined_gpx_attributes["max_lon"] - combined_gpx_attributes["min_lon"])
-        * margin_percent
-        * (-1 if combined_gpx_attributes["max_lon"] < 0 else 1)
-    )
-
-    plot_border_y_min = combined_gpx_attributes["min_lat"] - (
-        (combined_gpx_attributes["max_lat"] - combined_gpx_attributes["min_lat"])
-        * margin_percent
-        * (-1 if combined_gpx_attributes["min_lat"] < 0 else 1)
-    )
-    plot_border_y_max = combined_gpx_attributes["max_lat"] + (
-        (combined_gpx_attributes["max_lat"] - combined_gpx_attributes["min_lat"])
-        * margin_percent
-        * (-1 if combined_gpx_attributes["max_lat"] < 0 else 1)
-    )
+    ax.cla()
 
     ax.set_xlim(plot_border_x_min, plot_border_x_max)
     ax.set_ylim(plot_border_y_min, plot_border_y_max)
     ax.axis("off")
     ax.tick_params(left=False, labelleft=False, bottom=False, labelbottom=False)
 
-    # print(retval)
     cx.add_basemap(
         ax,
-        crs=gdf.crs.to_string(),
+        crs=crs_projection,
         attribution="",
     )
+
+    current_lats = combined_gpx_attributes["lats"][idx]
+    current_lons = combined_gpx_attributes["lons"][idx]
+
+    plot_points(
+        ax,
+        after_1_previous_tracks["lats"],
+        after_1_previous_tracks["lons"],
+        "#e59866",
+    )
+    plot_points(
+        ax,
+        within_1_previous_tracks["lats"],
+        within_1_previous_tracks["lons"],
+        "#d35400",
+    )
+    plot_points(
+        ax,
+        current_lats,
+        current_lons,
+        "#a04000",
+    )
+
 
     # save image
     print(
@@ -122,13 +139,15 @@ for idx in range(0, combined_gpx_attributes["last_step"], step):
     )
     image_file_path = f"{images_folder}/foo{idx}.png"
     plt.savefig(image_file_path, bbox_inches="tight", pad_inches=0)
-    plt.close()
-
     file_names.append(image_file_path)
 
-    # move current tracks into previous tracks
-    previous_tracks["lats"].extend(current_lats)
-    previous_tracks["lons"].extend(current_lons)
+    # prep for next round
+    after_1_previous_tracks["lats"].extend(within_1_previous_tracks["lats"])
+    after_1_previous_tracks["lons"].extend(within_1_previous_tracks["lons"])
+    within_1_previous_tracks["lats"] = current_lats
+    within_1_previous_tracks["lons"] = current_lons
+
+plt.close()
 
 
 gif_path = "./gpx_combined.gif"
@@ -136,3 +155,5 @@ gif_path = "./gpx_combined.gif"
 frames = np.stack([iio.imread(filename) for filename in file_names], axis=0)
 iio.imwrite(gif_path, frames)
 optimize(gif_path)
+
+print(time.time() - start_time, "seconds")
